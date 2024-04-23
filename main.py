596744937e658ed6938bfdd5cbae5b1eb4f5b2c3
Aasees <wo5dsmith@gmail.com>
2024-04-23 13:45:21 -0400

    initialized tkinter

--- main.py
+++ main.py
@@ -7,3 +7,23 @@
 # - new game button -> istantiate gameEngine object
 # - quit game button -> closes screen
 # - create a gameEngine object
+import tkinter as tk
+
+class Welcome:
+    def __init__(self):
+        # initialize
+        self.root = tk.Tk()
+        self.root.geometry('500x500')
+        self.root.configure(bg = 'white')
+        self.root.title('Welcome Page')
+
+        # creating labels
+        self.title = tk.Label(self.root, text = 'Tic Tac Toe', font = ('Arial', 30))
+
+        # placing labels
+        self.title.grid(row = 0, column = 0, padx= 0, pady= 0)
+
+        self.root.mainloop()
+
+
+w1 = Welcome()
