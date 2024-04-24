8c9486bdf8fd99483613c35e4bb6f5fb644eb80c
Muazzam Waheed <136852644+MuazzamW@users.noreply.github.com>
2024-04-24 13:35:33 +0000

    finished game engine

--- gameEngine.py
+++ gameEngine.py
@@ -1,3 +1,6 @@
+import random
+import json
+
 #class gameEngine
 # Description: This class is the main engine for the game, makes all decisions
 
@@ -13,8 +16,76 @@
 #functions:
 # - check winner
 # - make move (accepts coordinates from the gui class)
-# - reset game function (return a new instance of the gameEngine class)
+# - reset game function sets all parameters to zero
 # - write to game log
 
 class gameEngine:
-    pass
+    
+    def __init__(self,xPlayer, oPlayer):
+        self.__xPlayer = xPlayer
+        self.__oPlayer = oPlayer
+        self.reset()
+        
+    
+    def reset(self):
+        self.__grid = [[None, None, None], [None, None, None], [None, None, None]]
+        self.__turn = random.choice(['X', 'O'])
+        self.__move_counter = 0
+        self.__game_over = False
+    
+    def makeMove(self,xPos, yPos):
+        if not self.__grid[yPos][xPos] is None:
+            self.__grid[yPos][xPos] = self.__turn
+        else:
+            raise LocationError("Invalid location, please try again.") 
+        self.__move_counter += 1
+        self.__turn = "X" if self.__turn == "O" else "O"
+    
+    def checkWinner(self):
+        #check rows
+        for row in self.__grid:
+            if row[0] == row[1] == row[2] and row[0] is not None:
+                self.__game_over = True
+                return (row[0],self.__game_over)
+        
+        #check columns
+        for i in range(3):
+            if self.__grid[0][i] == self.__grid[1][i] == self.__grid[2][i] and self.__grid[0][i] is not None:
+                self.__game_over = True
+                return (self.__grid[0][i], self.__game_over)
+        
+        #check diagonals
+        if self.__grid[0][0] == self.__grid[1][1] == self.__grid[2][2] and self.__grid[0][0] is not None:
+            self.__game_over = True
+            return (self.__grid[0][0],self.__game_over)
+        
+        if self.__grid[0][2] == self.__grid[1][1] == self.__grid[2][0] and self.__grid[0][2] is not None:
+            self.__game_over = True
+            return (self.__grid[0][2],self.__game_over)
+        
+        if self.__move_counter == 9:
+            self.__game_over = True
+            return ("Tie",self.__game_over)
+        
+        return None
+    
+    def writeToLog(self):
+        data = {
+            #format grid as 3x3 grid
+            "grid": f"""
+                    {self.__grid[0]}
+                    {self.__grid[1]}
+                    {self.__grid[2]} 
+                    """,
+            "winner": self.checkWinner()[0],
+            "turns": self.__move_counter,
+            "Player X": self.__xPlayer,
+            "Player O": self.__oPlayer
+        }
+        with open('data.json', 'a', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+class LocationError(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+
--- gameScreen.py
+++ gameScreen.py
@@ -6,3 +6,6 @@
 # - create the grid of buttons
 # - add a reset button at the bottom
 # - quit game button
+
+#create button class that inherits from tk.Button
+#class has separate x and y coordinate
