71609e76d19bcfb760761f58f447c1b524105ce7
Aasees <wo5dsmith@gmail.com>
2024-04-26 08:32:02 -0400

    creating screen to ask user input for names

--- main.py
+++ main.py
@@ -36,9 +36,36 @@ class Welcome:
         self.root.mainloop()
 
     def startGame(self):
+        # initialize window that asks for user names
+        self.top = tk.Toplevel(self.root)
         
+        # variables for entries
+        self.player1 = tk.StringVar()
+        self.player2 = tk.StringVar()
+
+        # creating labels, entries, button
+        self.prompt1 = tk.Label(self.top, text = "Enter player 1's name: ")
+        self.prompt2 = tk.Label(self.top, text = "Enter player 2's name: ")
+        self.entry1 = tk.Entry(self.top, bd = 2)
+        self.entry2 = tk.Entry(self.top, bd = 2)
+        self.button1 = tk.Button(self.top, text = 'Continue', command = lambda:self.continueGame())
+
+        # grid labels, entries, buttons
+        self.prompt1.grid(row = 0, column = 0, padx = 5, pady = 5)
+        self.prompt2.grid(row = 1, column = 0, padx = 5, pady = 5)
+        self.entry1.grid(row = 0, column = 1, padx = 5, pady = 5)
+        self.entry2.grid(row = 1, column = 1, padx = 5, pady = 5)
+        self.button1.grid(row = 2, column = 1, padx = 5, pady = 5)
+
+
+    def continueGame(self):
+        # assign player names to variables
+        self.player1 = self.entry1.get()
+        self.player2 = self.entry2.get()
+
+        # close current screen and open game
         self.root.destroy()
-        g1 = gameScreen()
+        g1 = gameScreen(self.player1, self.player2)
 
     def viewGameLog(self):
         # ----- have gamelog be toplevel becuase we don't want to withdraw this class
